4def4a32c5c13ff7ebaa735b26ab4e3ac60d316a
Ali Elmi <ali.elmi77@yahoo.com>
2025-03-28 02:51:44 +0330

    add base repository implementation; create generic methods for database operations

---
+++ src/data/db/repository/shop_repo.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"context"
+	"log"
+
+	"github.com/alielmi98/go-ecommerce-api/constants"
+	"github.com/alielmi98/go-ecommerce-api/data/db"
+	"gorm.io/gorm"
+)
+
+const softDeleteExp string = "id = ? and deleted_by is null"
+
+type BaseRepository[TEntity any] struct {
+	database *gorm.DB
+}
+
+func NewBaseRepository[TEntity any]() *BaseRepository[TEntity] {
+	return &BaseRepository[TEntity]{
+		database: db.GetDb(),
+	}
+}
+
+func (r BaseRepository[TEntity]) Create(ctx context.Context, entity TEntity) (TEntity, error) {
+	tx := r.database.WithContext(ctx).Begin()
+	err := tx.
+		Create(&entity).
+		Error
+	if err != nil {
+		tx.Rollback()
+		log.Printf("Caller:%s Level:%s Msg:%s", constants.Postgres, constants.Insert, err.Error())
+		return entity, err
+	}
+	tx.Commit()
+	return entity, nil
+}
